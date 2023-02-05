cf95b52b528acd11a38b97259bc2a6bcfd14d7c5
Vanessa <vanessa.co403@gmail.com>
2023-02-05 13:21:46 -0500

    task lister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#create-task-form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const inputTask = document.querySelector('#new-task-description').value;
+    const tasks = document.querySelector("#tasks");
+    console.log(tasks);
+
+    const newLi = document.createElement("li");
+
+    newLi.innerHTML = inputTask;
+    console.log(newLi);
+    tasks.appendChild(newLi);
+  });
 });
+
+ 
